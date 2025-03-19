a372df4a4ab063d64bd113eef4c74e414e1fb78a
CaioVictor <caiorbcavalcante@gmail.com>
2025-03-18 22:16:40 -0300

    Aprendendo módulos customizados

---
+++ Section11/Modulos_Customizados.py
@@ -0,0 +1,17 @@
+"""
+Módulos customizados
+
+Como módulos Python nada mais são que arquivos, então todo os arquivos que criamos no curso são módulos Python prontos para serem utilizados.
+
+Você consegue importar módulos que criamos para outros arquivos. Exemplo:
+
+# Importando uma função específica
+
+from funcoes_com_parametros import soma_impares
+
+print(soma_impares([1, 2, 3, 4, 5, 6, 7]))
+
+# Importando todo o módulo
+import funcoes_com_parametros as fcp
+
+"""
