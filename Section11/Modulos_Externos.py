2e5b064982958c96ba292f9759581a068deed38c
caio <caiorbcavalcante@gmail.com>
2025-03-19 21:27:02 -0300

    Anotações breves sobre módulos externos em python.

---
+++ Section11/Modulos_Externos.py
@@ -0,0 +1,17 @@
+"""
+Modulos Externos
+
+Utilizamos o gerenciador de pacotes Python chamado Pip - Python Installer Package
+
+Você pode conhecer todos os pacotes externos oficiais em: https://pypi.org
+
+colorama -> É utilizado para permitir impressão de textos coloridos no terminal
+
+Instalando um módulo
+
+pip install nome_do_modulo
+"""
+
+from colorama import Fore, Back, Style
+
+print(Style.RESET_ALL)
